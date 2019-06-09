6e03c3d98d9d51f78e96a557dabc6508d49ffebd
Alexander Welshtein <alexanderwelshtein@gmail.com>
2019-06-09 14:54:56 +0400

    Create project configuration

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+// noinspection JSUnresolvedFunction, NodeJsCodingAssistanceForCoreModules
+const path = require('path');
+
+// noinspection JSUnresolvedVariable
+module.exports = {
+    entry: './sources/main.ts',
+    mode: "development",
+    devtool: "inline-source-map",
+
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'generated/[name].[ext]',
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(svg)$/,
+                loader: 'raw-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    output: {
+        filename: 'generated/main.js',
+        path: path.resolve(__dirname, './')
+    }
+};
