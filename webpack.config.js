bdd1e76e1c230966c93673be7bb20c8187f7f37d
Alexander Welshtein <alexanderwelshtein@gmail.com>
2020-03-25 10:25:48 +0400

    Actualize project configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,55 @@
 // noinspection JSUnresolvedFunction, NodeJsCodingAssistanceForCoreModules
-const path = require('path');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 // noinspection JSUnresolvedVariable
 module.exports = {
-    entry: './source/main.ts',
-    mode: "development",
+    entry: "./src/index.ts",
     devtool: "inline-source-map",
-
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        watchContentBase: true,
+        progress: true,
+        port: 3000,
+        hot: true,
+        open: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    })],
     module: {
-        rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        },
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
             {
                 test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ],
+                use: ["style-loader", "css-loader", "sass-loader"],
                 exclude: /node_modules/
             },
             {
                 test: /\.(jpg|png)$/,
-                loader: 'file-loader',
+                loader: "file-loader",
                 options: {
-                    name: 'generated/[name].[ext]',
+                    name: "[name].[ext]"
                 },
                 exclude: /node_modules/
             },
             {
-                test: /\.(svg)$/,
-                loader: 'raw-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(txt)$/,
-                loader: 'raw-loader',
+                test: /\.(svg|txt)$/,
+                loader: "raw-loader",
                 exclude: /node_modules/
             }
         ]
-    },
-
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-
-    output: {
-        filename: 'generated/bundle.js',
-        path: path.resolve(__dirname, './')
     }
-};
+}
